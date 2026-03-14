9f112a12a3adefb27feff78d351122df15f5ea12
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-03-14 18:30:11 +0530

    Add AccountRepository with add and remove methods

---
+++ src/main/java/com.repository/AccountRepository.java
@@ -0,0 +1,51 @@
+package com.repository;
+
+import java.util.Scanner;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import com.database.HibernateConnection;
+import com.entity.Account;
+
+public class AccountRepository {
+	EntityManager em = HibernateConnection.getEntityManager();
+	EntityTransaction et = em.getTransaction();
+
+	public void addAccount() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Account No.: ");
+		String acc_no = sc.nextLine();
+		sc.nextLine();
+		System.out.print("Enter Account Type: ");
+		String acc_type = sc.nextLine();
+		System.out.print("Enter Account Balance: ");
+		double balance = sc.nextDouble();
+		Account ac1 = new Account();
+		ac1.setAccount_no(acc_no);
+		ac1.setAcc_Type(acc_type);
+		ac1.setBalance(balance);
+		sc.close();
+		et.begin();
+		em.persist(ac1);
+		et.commit();
+	}
+
+	public void removeAccount() {
+		EntityManager em = HibernateConnection.getEntityManager();
+		EntityTransaction et = em.getTransaction();
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Account No.: ");
+		String acc_no = sc.nextLine();
+		sc.nextLine();
+		Account ac1 = em.find(Account.class, acc_no);
+		if (ac1 != null) {
+			em.remove(ac1);
+		} else {
+			System.out.println("Account doesn't exists");
+		}
+		et.begin();
+		em.persist(ac1);
+		et.commit();
+	}
+}
