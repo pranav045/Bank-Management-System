d849ea1c90a488a6b28159b22907710636d2c776
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-03-16 21:01:04 +0530

    Implement addBank method in BankRepository

--- src/main/java/com.repository/BankRepository.java
+++ src/main/java/com.repository/BankRepository.java
@@ -1,1 +1,36 @@
+package com.repository;
+
+import java.util.Scanner;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import com.database.HibernateConnection;
+import com.entity.Bank;
+
+public class BankRepository {
+	public void addBank() {
+		EntityManager em = HibernateConnection.getEntityManager();
+		EntityTransaction et = em.getTransaction();
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Bank Id: ");
+		String id = sc.nextLine();
+		System.out.print("Enter Bank name: ");
+		String name = sc.nextLine();
+		System.out.print("Enter Bank Code: ");
+		String code = sc.nextLine();
+		System.out.print("Enter Bank address: ");
+		String address = sc.nextLine();
+		Bank b1 = new Bank();
+		b1.setId(id);
+		b1.setName(name);
+		b1.setCode(code);
+		b1.setAddress(address);
+		sc.close();
+		et.begin();
+		em.persist(b1);
+		et.commit();
+	}
+
+}
 
