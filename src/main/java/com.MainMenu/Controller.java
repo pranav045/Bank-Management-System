46bfd589a0ad837bbab2372ef927fa3bed9916bf
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-03-19 21:48:07 +0530

    Refactor Controller to include BankRepository

--- src/main/java/com.MainMenu/Controller.java
+++ src/main/java/com.MainMenu/Controller.java
@@ -3,23 +3,46 @@ package com.MainMenu;
 import java.util.Scanner;
 
 import com.repository.AccountRepository;
+import com.repository.BankRepository;
 
 public class Controller {
 	public static void main(String[] args) {
-		AccountRepository repo = new AccountRepository();
+		AccountRepository account = new AccountRepository();
+		BankRepository Bank = new BankRepository();
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Welcome to the Bank Management System");
+		System.out.println("How may I help you?");
+		System.out.println("1. Account Settings");
+		System.out.println("2. Bank settings");
 		System.out.print("Enter option: ");
 		int option = sc.nextInt();
 		switch (option) {
 		case 1:
-			repo.addAccount();
+			System.out.print("Enter option: ");
+			int option2 = sc.nextInt();
+			switch (option2) {
+			case 1:
+				account.addAccount();
+				break;
+			case 2:
+				account.removeAccount();
+				break;
+			}
 			break;
 		case 2:
-			repo.removeAccount();
+			System.out.println("1. Add Bank");
+			System.out.println("2. Remove Bank");
+			System.out.print("Enter option: ");
+			int option3 = sc.nextInt();
+			switch (option3) {
+			case 1:
+				Bank.addBank();
+				break;
+			case 2:
+				Bank.removeBank();
+				break;
+			}
 			break;
 		}
-		;
-		System.out.println("1. Add Account");
-		System.out.println("2. Remove Account");
 	}
 }
