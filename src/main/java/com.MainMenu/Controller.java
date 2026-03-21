493c631c27623e52659f4f54b4a1c41c313c99f1
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-03-21 19:11:26 +0530

    Update Controller.java with customer settings

--- src/main/java/com.MainMenu/Controller.java
+++ src/main/java/com.MainMenu/Controller.java
@@ -5,18 +5,21 @@ import java.util.Scanner;
 import com.repository.AccountRepository;
 import com.repository.BankRepository;
 import com.repository.BranchRepository;
+import com.repository.CustomerRepository;
 
 public class Controller {
 	public static void main(String[] args) {
 		AccountRepository account = new AccountRepository();
 		BankRepository Bank = new BankRepository();
 		BranchRepository Branch = new BranchRepository();
+		CustomerRepository Customer = new CustomerRepository();
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Welcome to the Bank Management System");
 		System.out.println("How may I help you?");
 		System.out.println("1. Account Settings");
 		System.out.println("2. Bank settings");
 		System.out.println("3. Branch settings");
+		System.out.println("4. Customer settings");
 		System.out.print("Enter option: ");
 		int option = sc.nextInt();
 		switch (option) {
@@ -60,7 +63,22 @@ public class Controller {
 				break;
 			}
 			break;
+		case 4:
+			System.out.println("1. Add Customer");
+			System.out.println("2. Remove Customer");
+			System.out.print("Enter option: ");
+			int option5 = sc.nextInt();
+			switch (option5) {
+			case 1:
+				Customer.addCustomer();
+				break;
+			case 2:
+				Customer.removeCustomer();
+				break;
+			}
+			break;
 		}
+
 		sc.close();
 	}
 }
