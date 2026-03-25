8377368e88cb72f3d77a0e3ba8f5c9c54f6c6c64
Pranav Gaira <148996644+pranav045@users.noreply.github.com>
2026-03-25 20:00:23 +0530

    Update AccountRepository.java with the customer adding

--- src/main/java/com.repository/AccountRepository.java
+++ src/main/java/com.repository/AccountRepository.java
@@ -7,6 +7,7 @@ import javax.persistence.EntityTransaction;
 
 import com.database.HibernateConnection;
 import com.entity.Account;
+import com.entity.Customer;
 
 public class AccountRepository {
 	EntityManager em = HibernateConnection.getEntityManager();
@@ -17,6 +18,7 @@ public class AccountRepository {
 		System.out.print("Enter Account No.: ");
 		String acc_no = sc.nextLine();
 		Account ac1 = em.find(Account.class, acc_no);
+		et.begin();
 		if (ac1 == null) {
 			ac1 = new Account();
 			ac1.setAccount_no(acc_no);
@@ -26,15 +28,35 @@ public class AccountRepository {
 			double balance = sc.nextDouble();
 			ac1.setAcc_Type(acc_type);
 			ac1.setBalance(balance);
+			System.out.println("Enter Customer id: ");
+			int cust_id = sc.nextInt();
+			Customer customer = em.find(Customer.class, cust_id);
+			if (customer == null) {
+				customer = new Customer();
+				customer.setCustid(cust_id);
+				System.out.println("Enter customer name: ");
+				String name = sc.nextLine();
+				System.out.println("Enter customer address: ");
+				String address = sc.nextLine();
+				System.out.println("Enter phone no.: ");
+				String phone = sc.nextLine();
+				customer.setName(name);
+				customer.setAddress(address);
+				customer.setPhone(phone);
+				ac1.setCustomer(customer);
+			} else {
+				ac1.setCustomer(customer);
+			}
+			em.persist(customer);
+
 		} else {
 			System.out.println("Account already exists");
 		}
 		sc.close();
-		et.begin();
 		if (em.find(Account.class, acc_no) == null) {
 			em.persist(ac1);
+			et.commit();
 		}
-		et.commit();
 		sc.close();
 	}
 
